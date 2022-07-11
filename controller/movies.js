0a1e957977cae3467c702241183d23ca8c6a0b8c
Permana00 <nokushirin@gmail.com>
2022-07-11 12:58:55 +0700

    fix movies.js controller

--- controller/movies.js
+++ controller/movies.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
 
 module.exports = {
   getAll: async (req, res) => {
+    const id = req.params.id;
     axios
-      .get(
-        `https://api.themoviedb.org/3/movie/76341?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f`
-      )
+      .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
       .then((response) => {
         res.status(200).json({ data: response.data });
       })
@@ -19,7 +20,7 @@ module.exports = {
     const genreId = req.params.genreId;
     axios
       .get(
-        `https://api.themoviedb.org/3/discover/movie?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreId}&with_watch_monetization_types=flatrate`
+        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreId}&with_watch_monetization_types=flatrate`
       )
       .then((response) => {
         res.status(200).json({ data: response.data.results });
@@ -32,7 +33,7 @@ module.exports = {
   getGenre: async (req, res) => {
     axios
       .get(
-        `https://api.themoviedb.org/3/genre/movie/list?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data.genres });
@@ -43,9 +44,10 @@ module.exports = {
   },
 
   getTopAiring: async (req, res) => {
+    const page = req.params.page;
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US&page=1`
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=${page}`
       )
       .then((response) => {
         res.status(200).json({ data: response.data.results });
@@ -59,7 +61,7 @@ module.exports = {
     const id = req.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data });
@@ -73,7 +75,7 @@ module.exports = {
     const id = req.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data });
@@ -87,7 +89,7 @@ module.exports = {
     const id = req.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data });
@@ -99,9 +101,10 @@ module.exports = {
 
   getSearch: async (req, res) => {
     const query = req.params.query;
+    const page = req.params.page;
     axios
       .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US&query=${query}&page=1&include_adult=false`
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=${page}&include_adult=false`
       )
       .then((response) => {
         res.status(200).json({ data: response.data.results });
@@ -115,7 +118,7 @@ module.exports = {
     const page = req.params.page;
     axios
       .get(
-        `https://api.themoviedb.org/3/discover/tv?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US&sort_by=popularity.desc&page=${page}1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+        `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=${page}&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
       )
       .then((response) => {
         res.status(200).json({ data: response.data.results });
@@ -129,7 +132,7 @@ module.exports = {
     const id = req.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/tv/${id}?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data });
@@ -143,7 +146,7 @@ module.exports = {
     const id = req.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/tv/${id}/credits?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data });
@@ -157,7 +160,7 @@ module.exports = {
     const id = req.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/tv/${id}/videos?api_key=a3a7d71dfb4ae09ac97bb78ec237c46f&language=en-US`
+        `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${apiKey}&language=en-US`
       )
       .then((response) => {
         res.status(200).json({ data: response.data });
--- routes/movies.js
+++ routes/movies.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const moviesController = require("../controller/movies");
 
 router.get("/get_movies/:genreId/:page", moviesController.getMovies);
-router.get("/get_top", moviesController.getTopAiring);
+router.get("/get_top/:page", moviesController.getTopAiring);
 router.get("/get_genre", moviesController.getGenre);
 router.get("/get_detail/:id", moviesController.getDetail);
 router.get("/get_casting/:id", moviesController.getCasting);
 router.get("/get_trailer/:id", moviesController.getTrailer);
-router.get("/get_search/:query", moviesController.getSearch);
+router.get("/get_search/:query/:page", moviesController.getSearch);
 router.get("/get_tvseries/:page", moviesController.getTvSeries);
 router.get("/get_one_tvseries/:id", moviesController.getOneTvSeries);
 router.get("/get_casting_tv/:id", moviesController.getCastingTv);
